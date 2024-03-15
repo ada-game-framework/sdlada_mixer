1640c5e8b950c42ab3f2a275c3c96bebef1e0cf3
Luke A. Guest <laguest@archeia.com>
2024-03-15 18:38:04 +0000

    Add source from SDLAda. Fix spelling of Volume.

---
+++ src/link/nix/sdl-mixer_linker.ads
@@ -0,0 +1,10 @@
+--------------------------------------------------------------------------------------------------------------------
+--  This source code is subject to the Zlib license, see the LICENCE file in the root of this directory.
+--------------------------------------------------------------------------------------------------------------------
+--  SDL.Mixer.Linker
+--------------------------------------------------------------------------------------------------------------------
+package SDL.Mixer_Linker is
+   pragma Pure;
+
+   Mixer_Linker_Options : constant String := "-lSDL2_mixer";
+end SDL.Mixer_Linker;
